1c9961e12b75a1733aeb5dfeb4480848eda6650d
Attila Orosz <attila.orosz.dev@gmail.com>
2025-08-12 19:51:28 +0400

    Th missing files, as always...

--- ecommerce_rtk/src/Components/ProductList.jsx
+++ ecommerce_rtk/src/Components/ProductList.jsx
@@ -1,22 +1,49 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
-import './ProductList.css'; 
+import {useDispatch, useSelector} from 'react-redux';
+import {addItemToCart} from './CartSlice';
+import './ProductList.css';
 
 const ProductList = () => {
 
-  const products = [
-    { id: 1, name: 'Product A', price: 60 },
-    { id: 2, name: 'Product B', price: 75 },
-    { id: 3, name: 'Product C', price: 30 },
-  ];
+    const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart.cartItems); //Get cart items globally
 
-  return (
-    <div className="product-list">
-      <h2 className="product-list-title">Products</h2>
-      <ul className="product-list-items">
-     
-      </ul>
-    </div>
-  );
+    const products = [
+        {id: 1, name: 'Product A', price: 60},
+        {id: 2, name: 'Product B', price: 75},
+        {id: 3, name: 'Product C', price: 30},
+    ];
+
+    const handleAddToCart = (product) => {
+        dispatch(addItemToCart(product));
+    };
+
+    return (
+        <div className="product-list">
+            <h2 className="product-list-title">Products</h2>
+            <ul className="product-list-items">
+                {products.map(product => {
+                    const isAlreadyInCart = cartItems.some(item => item.id === product.id);
+
+                    return (
+                        <li key={product.id} className="product-list-item">
+                            <span>{product.name}${product.price}</span>
+                            <button className={`add-to-cart-btn ${isAlreadyInCart ? 'disabled' : ''}`}
+                                // onClick={() => dispatch(addItemToCart(product))}>
+                                    onClick={() => handleAddToCart(product)}
+                                    disabled={isAlreadyInCart} //Disable if already in cart
+                            >
+                                {isAlreadyInCart ? 'Added' : 'Add to Cart'}
+                            </button>
+                        </li>
+                    );
+
+                })}
+            </ul>
+        </div>
+    );
+
 };
 
 export default ProductList;
