b27f16ee111972f9471cfcf8d459059b66df6152
Attila Orosz <attila.orosz.dev@gmail.com>
2025-08-13 19:21:02 +0400

    Moved the conference event planner practice lab from its own repo to this collective garbage bin

---
+++ conference_event_planner/src/ItemsDisplay.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./ConferenceEvent.css";
+
+const ItemsDisplay = ({items, numberOfPeople}) => {
+  console.log(items);
+  return <>
+    <div className="display_box1">
+      {items.length === 0 ? (
+        <h3 style={{ textAlign: "center", paddingTop: "20px", fontSize: "25px" }}>No items selected</h3>
+      ) : (
+        <table className="table_item_data">
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Unit Cost</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+          </tr>
+          </thead>
+          <tbody>
+          {items.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>${item.cost}</td>
+              <td>
+                {item.type === "meals" || item.numberOfPeople
+                  ? ` For ${numberOfPeople} people`
+                  : item.quantity}
+              </td>
+              <td>{item.type === "meals" || item.numberOfPeople
+                ? `${item.cost * numberOfPeople}`
+                : `${item.cost * item.quantity}`}
+              </td>
+            </tr>
+          ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  </>
+
+};
+
+export default ItemsDisplay;
